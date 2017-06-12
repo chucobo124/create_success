312b39129c01f26e6e1d7ac649ed18700c6c0cf0
ChenLucas <chucobo5219@gmail.com>
2017-06-13 01:39:32 +0800

    refactor images

--- app/controllers/concerns/imageable.rb
+++ app/controllers/concerns/imageable.rb
@@ -2,9 +2,7 @@ module Imageable
   extend ActiveSupport::Concern
   def pictures_params(main_key)
     images = params.require(main_key).require(:pictures)
-    images.values.map.with_index do |image, index|
-      { asset: image }
-    end
+    images.values.map { |image| { asset: image } }
   end
 
   def save_images(klass, picture_object)

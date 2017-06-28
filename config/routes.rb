f47ac995900ae5c9de89c8535d841c385350d8ed
ChenLucas <chucobo5219@gmail.com>
2017-06-28 22:00:26 +0800

    create form container

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'home#index'
     resources 'products'
+    resources 'news'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612205338) do
+ActiveRecord::Schema.define(version: 20170628131908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.integer  "imageable_id"

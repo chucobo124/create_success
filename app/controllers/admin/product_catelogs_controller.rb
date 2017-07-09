2fdc734f204211bd0b154fdd7ceb1d38608b8db4
ChenLucas <chucobo5219@gmail.com>
2017-07-09 19:23:26 +0800

    create product category page

--- app/controllers/admin/product_catelogs_controller.rb
+++ app/controllers/admin/product_catelogs_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductCatelogsController < Admin::ApplicationController
+  before_action :product_catelog, only: [:show, :update, :delete]
   def index
     @admin_product_catelogs = ProductCatelog.all
   end
@@ -8,12 +9,25 @@ class Admin::ProductCatelogsController < Admin::ApplicationController
     @redirect_path = admin_product_catelogs_path
   end
 
+  def show
+    @redirect_path = admin_product_catelog_path
+  end
+
   def create
     ProductCatelog.create(product_catelog_params)
     redirect_to admin_product_catelogs_path
   end
 
+  def update
+    @admin_product_catelog.update(product_catelog_params)
+    redirect_to admin_product_catelogs_path
+  end
+
   private
+
+  def product_catelog
+    @admin_product_catelog = ProductCatelog.find(params[:id])
+  end
 
   def product_catelog_params
     params.require(:product_catelog).permit(:title, :description, :file_path)

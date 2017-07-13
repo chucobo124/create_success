3ef9f51863f8a263955814de50229c550b986830
ChenLucas <chucobo5219@gmail.com>
2017-07-13 22:13:52 +0800

    style login page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile +=
   %w( navigation.css home.css product_detail.css about_us.css file_download.css
   footer.css admin/home.css side_bar.css admin/product_form.css product_catelogs.css top_navgation.js
-  home_adv_animation.js contact_us.css about_us_page.css faq.css news.css  product_page.css home_product_page.css)
+  home_adv_animation.js contact_us.css about_us_page.css faq.css news.css  product_page.css home_product_page.css
+  admin/oauth.css)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
